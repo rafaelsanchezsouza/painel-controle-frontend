cfffcbe1e76c0c056173e12548212a943b87d789
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-05-11 15:43:17 -0300

    hello world!

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.png" alt="SP Consig"></img>
+
+      <p>Tecnologia Simples</p>
+
+      <span>Qui, 8 de Abril</span>
+    </header>
+  );
+}
