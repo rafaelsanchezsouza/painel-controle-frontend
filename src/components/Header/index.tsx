ee809924e8b436566fa38035c60401dbf2f98920
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-05-11 17:52:01 -0300

    feat: implement dinamic date

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
 import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const currentDate = format(new Date(), 'EEEE, d MMMM', {
+    locale: ptBR,
+  });
+
   return (
     <header className={styles.headerContainer}>
       <img src="/logo.png" alt="SP Consig"></img>
 
       <p>Tecnologia Simples</p>
 
-      <span>Qui, 8 de Abril</span>
+      <span>{currentDate}</span>
     </header>
   );
 }
