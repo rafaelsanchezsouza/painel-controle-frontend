cb7e75b0e8a313b260f6c24df973d5e45f89de4c
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-05-12 09:49:34 -0300

    feat: implement SSR API consuming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
-import { Header } from '../components/Header';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
-  return <Header></Header>;
+// Components
+import { Header } from '../components/Header';
+import { Portal } from '../components/Portal';
+import next from 'next';
+
+// Variables
+// import getPortais from '../portais.json';
+
+export default function Home(props) {
+  const [portais, setPortais] = useState([]);
+
+  return (
+    <>
+      <Header></Header>
+      {/* {portais.map((portal) => {
+        return <Portal portal={portal} />;
+      })} */}
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/portais');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+  };
 }
