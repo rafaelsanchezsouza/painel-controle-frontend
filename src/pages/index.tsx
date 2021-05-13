8d0c6fa100108e1c9a3369493fee7c5b7b82d750
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-05-13 10:17:33 -0300

    feat: implementa visualização simples de portais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import { GetServerSideProps } from 'next';
 import { useEffect, useState } from 'react';
+import api from '../service/api';
+import styles from './home.module.scss';
 
 // Components
-import { Header } from '../components/Header';
 import { Portal } from '../components/Portal';
-import next from 'next';
-
-// Variables
-// import getPortais from '../portais.json';
 
 // Types
 type HomeProps = {
@@ -16,30 +13,59 @@ type HomeProps = {
 
 type Portal = {
   id: string;
-  codigo: string;
-  nome: string;
+  cnpj: string;
+  nomeBase: string;
+  nomenclatura: string;
+  status: string;
 };
 
-export default function Home(props: HomeProps) {
-  const [portais, setPortais] = useState([]);
-
+export default function Home({ portais }: HomeProps) {
   return (
-    <>
-      <Header></Header>
-      {/* {portais.map((portal) => {
-        return <Portal portal={portal} />;
-      })} */}
-    </>
+    <div className={styles.homepage}>
+      <section className={styles.portais}>
+        <h2>Portais </h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th>Nomenclatura</th>
+              <th>Nome da Base</th>
+              <th>CNPJ</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {portais.map((portal: Portal) => {
+              return (
+                <tr key={portal.id}>
+                  <td className={styles.nomenclatura}>{portal.nomenclatura}</td>
+                  <td>
+                    <a
+                      href={`https://www.consigsimples.com.br/${portal.nomeBase}`}
+                    >
+                      {portal.nomeBase}
+                    </a>
+                  </td>
+
+                  <td>{portal.cnpj}</td>
+                  <td>{portal.status}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await fetch('http://localhost:3333/portais');
-  const data = await response.json();
+  const { data } = await api.get('portais');
 
   return {
     props: {
-      episodes: data,
+      portais: data,
     },
   };
 };
