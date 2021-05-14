586681868aa637e8e53e7c0c66b6f9d3cf62ea5a
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-05-14 16:27:47 -0300

    feat: implementa pagina com informacoes de portal

--- src/pages/portais/[nomeBase].tsx
+++ src/pages/portais/[nomeBase].tsx
@@ -16,7 +16,32 @@ export default function Portais() {
     <div className={styles.portal}>
       <h2>{router.query.nomeBase}</h2>
 
-      <h3>{portal.cnpj}</h3>
+      <section className={styles.infoPortal}>
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th>Nomenclatura</th>
+              <th>Nome da Base</th>
+              <th>CNPJ</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr key={portal.id}>
+              <td className={styles.nomenclatura}>dawdawdawdw</td>
+              <td>
+                <a href={`https://www.consigsimples.com.br/${portal.nomeBase}`}>
+                  {portal.nomeBase}
+                </a>
+              </td>
+
+              <td>{portal.cnpj}</td>
+              <td>{portal.status}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
 
       <Link href={'/'}>
         <button type="button">
